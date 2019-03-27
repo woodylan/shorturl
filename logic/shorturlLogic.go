1e2a7b9fde3889116e8bb0e6731c2fb8551dbf66
woodylan <woodylan@126.com>
2019-03-27 17:53:50 +0800

    Parse URL & insert into table

--- logic/shorturlLogic.go
+++ logic/shorturlLogic.go
@@ -5,6 +5,9 @@ import (
 	"github.com/speps/go-hashids"
 	"shorturl/modules/util"
 	"github.com/astaxie/beego"
+	"net/url"
+	"regexp"
+	"shorturl/models/shorturl"
 )
 
 type ShortUrlLogic struct {
@@ -15,9 +18,32 @@ type RetData struct {
 	Host     string `json:"host"`
 }
 
-func (this *ShortUrlLogic) Short(c *context.Context, url string) (retData RetData) {
+func (this *ShortUrlLogic) Short(c *context.Context, urlString string) (retData RetData) {
+	// 正则验证URL
+	regex := `(http[s]?|ftp):\/\/([^\/\.]+?)\..+\w$`
+	if m, _ := regexp.MatchString(regex, urlString); !m {
+		util.ThrowApi(c, -1, "不是合法的URL：")
+	}
+
+	// 解析URL
+	urlParse, _ := url.Parse(urlString)
+	host := urlParse.Host
+
+	// 通过发号器获取ID
 	redisKey := beego.AppConfig.String("redis_key")
 	shortUrlId := util.GetRedisNum(redisKey)
+
+	// 计算发号器ID HASH值
+	hashId := getHashId(shortUrlId)
+
+	// 存入数据库
+	shorturlModel.AddNew(shortUrlId, hashId, urlString, host)
+
+	retData.ShortUrl = hashId
+	return
+}
+
+func getHashId(id int64) string {
 
 	hashIdClass := hashids.NewData()
 	hashIdClass.Salt = "Salt"
@@ -25,13 +51,11 @@ func (this *ShortUrlLogic) Short(c *context.Context, url string) (retData RetDat
 
 	hashNew, _ := hashids.NewWithData(hashIdClass)
 	// 转码
-	hashId, _ := hashNew.EncodeInt64([]int64{shortUrlId})
+	hashId, _ := hashNew.EncodeInt64([]int64{id})
 
 	////解码
-	//d, _ := h.DecodeWithError(e)
+	//d, _ := hashNew.DecodeWithError(e)
 	//fmt.Println(d[0])
 
-	retData.ShortUrl = hashId
-
-	return
+	return hashId
 }
