ae3482f9098b2d79bd6280907e7905536201c3ff
woodylan <woodylan@126.com>
2019-04-23 09:27:24 +0800

    redis Custom configuration

--- db/redis/redis.go
+++ db/redis/redis.go
@@ -1,14 +1,20 @@
 package redis
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"github.com/astaxie/beego"
+)
 
 var RedisConnect *redis.Client
 
 func Connect() (redisConnect *redis.Client, err error) {
+	host := beego.AppConfig.String("redis_host")
+	port := beego.AppConfig.String("redis_port")
+	password := beego.AppConfig.String("redis_password")
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     host + ":" + port,
+		Password: password, // no password set
+		DB:       0,        // use default DB
 	})
 
 	_, err = client.Ping().Result()
