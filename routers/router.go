d52846e39f071fb21721f3c98699a6eb21dfc134
woodylan <woodylan@126.com>
2019-05-15 15:17:43 +0800

    complete routers

--- routers/router.go
+++ routers/router.go
@@ -10,12 +10,24 @@ package routers
 import (
 	"shorturl/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"shorturl/modules/util"
 )
 
 func init() {
+	beego.Get("/", func(ctx *context.Context) {
+		retData := util.RetData{Code: 200, Msg: "Hello"}
+		ctx.Output.JSON(retData, true, false)
+	})
+
 	beego.Router("/api/v1/create", &controllers.CreateController{}, "*:Run")
 	beego.Router("/api/v1/query", &controllers.QueryController{}, "*:Run")
-	beego.Router("/?:url", &controllers.JumpController{}, "*:Run")
+	beego.Router("/:url([a-zA-z0-9]{6,})", &controllers.JumpController{}, "*:Run")
+
+	beego.Get("/*", func(ctx *context.Context) {
+		retData := util.RetData{Code: 404, Msg: "Not Found"}
+		ctx.Output.JSON(retData, true, false)
+	})
 
 	// 中间件过滤
 	beego.InsertFilter("/api/*", beego.BeforeRouter, filterLoggedInUser)
